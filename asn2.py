6e58884d6f00a5fb42769fd290cd112563f18211
adsantos97 <asantos@cs.loyola.edu>
2018-04-12 23:23:22 -0400

    python

--- asn2.py
+++ asn2.py
@@ -2,6 +2,21 @@ _author_ = 'arizza santos'
 
 import random
 
-def generateNum(n):
-  for x in range(n):
-    print random.randint(1, 101)
+def generate_num(n):
+    for x in range(n):
+        print random.randint(1, n+1)
+
+def main():
+    print("Generating random numbers")
+    generate_num(10)
+
+
+main()
+
+sentence = "the world is mine"
+words = sentence.split()
+word_lengths = []
+for word in words:
+  if word != "the":
+    word_lengths.append(len(word))
+print(word_lengths)
