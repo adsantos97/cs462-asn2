b5a815d01e233ea68fb1e5932f57e83a6621c194
adsantos97 <asantos@cs.loyola.edu>
2018-04-13 02:16:46 -0400

    done with max weight

--- asn2.py
+++ asn2.py
@@ -26,6 +26,11 @@ def generate_ints(start, n, step):
         ints.append(i)
     return ints
 
+# purpose: generate a random problem instance
+# input: start - starting integer
+#        n - numbre of objects
+#        step - increment between each integer
+# return: list of objects with their weights and values
 def generate_prob_instance(start, n, step):
     prob_instance = []
     obj = []
@@ -41,16 +46,34 @@ def generate_prob_instance(start, n, step):
         prob_instance.append(obj)
         print obj
         obj = []
-    return prob_instance
 
     # double check problem instance
     #for i, w, v in itertools.izip(objects, weights, values):
         #print("Object {}: {} {}".format(i, w, v))
+    return prob_instance
+
+# purpose: find the maximum weight that is possible to carry
+# input: prob_instance - list of objects
+# return: maximum weight (75% of the sum)
+def max_weight(prob_instance):
+    max_weight = 0    
+
+    for obj in prob_instance:
+        #for data in obj:
+        max_weight += obj[1]
+
+    #print max_weight
+    max_weight = int(round(max_weight * 0.75))
+    print max_weight
 
 def main():
     print("Generating random numbers")
-    list_ints = generate_ints(25, 100, 25)
+    start = 1
+    n = 10
+    step = 1
+    list_ints = generate_ints(start, n, step)
     print(list_ints)
-    generate_prob_instance(25, 100, 25)
+    problem = generate_prob_instance(start, n, step)
+    max_weight(problem)
 
 main()
